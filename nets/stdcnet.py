b26c23d9b0789400d55d3f36122ce282f2850303
SamsonKun <1392967285@qq.com>
2021-12-22 16:02:10 +0800

    run_lantency stdcnet

--- nets/stdcnet.py
+++ nets/stdcnet.py
@@ -3,8 +3,6 @@ import torch.nn as nn
 from torch.nn import init
 import math
 
-
-
 class ConvX(nn.Module):
     def __init__(self, in_planes, out_planes, kernel=3, stride=1):
         super(ConvX, self).__init__()
